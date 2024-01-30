beccda80804bffe733a0f5f0276dca6fcd3895e1
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2024-01-30 16:26:10 +0100

    Building a Role Playing Game
    Implementing a simple RPG game UI in HTML and JavaScript, enabling player navigation, actions, and dynamic interface updates based on choices. - INCOMPLETE

--- JavaScript-Algorithms-and-Data-Structures/Learn-Basic-JavaScript-by-Building-a-Role-Playing-Game/script.js
+++ JavaScript-Algorithms-and-Data-Structures/Learn-Basic-JavaScript-by-Building-a-Role-Playing-Game/script.js
@@ -160,11 +160,15 @@ function goFight() {
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
   monsterName.innerText = monsters[fighting].name;
-  monsterHealthText.innerText = monsters[fighting].health;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
 }
 
 function dodge() {}
