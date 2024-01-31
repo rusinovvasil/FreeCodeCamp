6a946151d261d8493a8c4284870ff3dd3d81587d
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2024-01-31 13:39:11 +0100

    Learn Form Validation by Building a Calorie Counter
    Incomplete

---
+++ JavaScript-Algorithms-and-Data-Structures/Learn-Form-Validation-by-Building-a-Calorie-Counter/script.js
@@ -0,0 +1,8 @@
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("enty-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+
+let isError = false;
